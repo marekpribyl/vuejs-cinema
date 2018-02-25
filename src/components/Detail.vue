720efc4e009e6db6ac578af3d5434d34c2bfcc19
Marek Pribyl <mr.pribyl@gmail.com>
2018-02-25 11:12:53 +0100

    Router extended, detail page component, slot added

---
+++ src/components/Detail.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="detail" v-if="movie">
+        <movie-item :movie="movie">
+            <div class="movie-details">
+                <p class="movie-genre">{{ movie.Genre }}</p>
+                <p class="movie-plot">{{ movie.plot }}</p>
+                <table>
+                    <tr><td>Release Date:</td><td>{{ movie.Released }}</td></tr>
+                    <tr><td>Running Time:</td><td>{{ movie.Runtime }}</td></tr>
+                    <tr><td>Director:</td><td>{{ movie.Director }}</td></tr>
+                    <tr><td>Cast:</td><td>{{ movie.Actors }}</td></tr>
+                </table>
+            </div>
+        </movie-item>
+        <div class="home">
+            <router-link :to="{ 'name': 'home'}">Back to list...</router-link>
+        </div>
+    </div>
+    <div v-else>No movie with provided ID</div>
+</template>
+
+<script>
+    import MovieItem from "./MovieItem.vue";
+
+    export default {
+        components: {MovieItem},
+        name: "detail",
+        props: [ 'movies', 'sessions', 'day', 'time' ],
+        computed: {
+          movie() {
+                let movie = this.movies.find(movie => movie.id === this.$route.params.id);
+                return movie ? movie.movie : null;
+          }
+        },
+        created() {
+            console.log(this.$route);
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,7 +1,13 @@
 <template>
     <div class="movie-list">
         <div v-if="filteredMovies.length">
-        <movie-item v-for="movie in filteredMovies" :movie="movie.movie" :sessions="movie.sessions" :day="day" :time="time"></movie-item>
+        <movie-item v-for="movie in filteredMovies" :movie="movie.movie">
+            <div class="movie-sessions">
+                <div v-for="session in filteredSessions(movie.sessions)" class="session-time-wrapper">
+                    <div class="session-time">{{ formatSessionTime(session.time) }}</div>
+                </div>
+            </div>
+        </movie-item>
         </div>
         <div v-else-if="movies.length" class="no-results">{{ noResults }}</div>
         <div v-else class="no-results">Loading...</div>
@@ -28,6 +34,23 @@
                     }
                 });
                 return match;
+            },
+            sessionPassesTimeFilter(session) {
+                if (!this.day.isSame(this.$moment(session.time), 'day')) {
+                    return false;
+                } else if (this.time.length === 0 || this.time.length === 2) {
+                    return true;
+                } else if (this.time[0] === times.AFTER_6PM) {
+                    return this.$moment(session.time).hour() >= 18;
+                } else {
+                    return this.$moment(session.time).hour() < 18;
+                }
+            },
+            formatSessionTime(raw) {
+                return this.$moment(raw).format('h:mm A');
+            },
+            filteredSessions(sessions) {
+                return sessions.filter(this.sessionPassesTimeFilter);
             },
             sessionPassesTimeFilter(session) {
                 if (!this.day.isSame(this.$moment(session.time), 'day')) {
